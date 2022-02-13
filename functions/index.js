6e4750030421ce580a1e7f01804f20a9461f9f3c
Erick <erguzman96@gmail.com>
2022-02-12 19:23:21 -0600

    feat(*): Get all products endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+const serviceAccount = require("./permissions.json");
+
+app.use(cors({ origin: true }));
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+});
+
+// Routes
+app.use(require("./routes/products.routes"));
+
+exports.app = functions.https.onRequest(app);
---
+++ functions/routes/products.routes.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const router = Router();
+
+const admin = require("firebase-admin");
+const db = admin.firestore();
+
+// Get all products
+router.get("/api/products", async (req, res) => {
+    try {
+        let query = db.collection("products");
+        const querySnapshot = await query.get();
+        let docs = querySnapshot.docs;
+
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            name: doc.data().name,
+        }));
+
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+});
+
+module.exports = router;
