2885d278619bb4974dfa0b1e955508abf0716213
Erick <erguzman96@gmail.com>
2022-02-15 16:57:19 -0600

    feat(*): Add Get products by term endpoint

--- functions/routes/products.routes.js
+++ functions/routes/products.routes.js
@@ -15,11 +15,35 @@ router.get("/api/products", async (req, res) => {
             id: doc.id,
             name: doc.data().name,
             price: doc.data().price,
+            image: doc.data().image,
         }));
 
         return res.status(200).json(response);
     } catch (error) {
         return res.status(500).json(error);
+    }
+});
+
+// Find products by name
+router.get("/api/products/:product_name", async (req, res) => {
+    try {
+        const doc = db.collection("products");
+
+        const items = await doc
+            .where("searchTerms", "array-contains", req.params.product_name)
+            .get();
+        let docs = items.docs;
+
+        const response = docs.map((doc) => ({
+            id: doc.id,
+            name: doc.data().name,
+            price: doc.data().price,
+            image: doc.data().image,
+        }));
+
+        return res.status(200).send(response);
+    } catch (error) {
+        return res.status(500).send(error);
     }
 });
 
